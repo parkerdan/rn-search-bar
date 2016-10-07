7a3b0777da63b9f69c4fdf6600e896d17149d92a
parkerdan <parker.daniel.o@gmail.com>
2016-10-07 10:48:17 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# RN-Search-Bar
+<br>
+### Uses `react-native-vector-icons/FontAwesome` as the icon source.
+<br>
+##### You must have `react-native-vector-icons` installed and linked for this to work
+<br>  
+
+* `npm install react-native-vector-icons --save`
+* `react-native link react-native-vector-icons`
+* `npm install rn-search-bar --save`
+
+
+
+
+
+```js
+import React, { Component } from 'react';
+
+import Header from 'rn-header';
+
+class SearchBarExampleOne extends Component {
+  render(){
+    return(
+      <SearchBar
+        onPress={
+          () => console.log('Do Stuff')
+        }
+        containerStyle={{
+          height:50
+        }}
+        clearTextOnPress={true}
+        renderLeftIcon={true}
+        searchIconSize={15}
+        searchIconColor={'deeppink'}
+        searchIconBackgroundColor={'aqua'}
+        leftIconColor={'rgba(152,152,152,.7)'}
+        leftIconSize={12}
+        textStyle={{
+          color: 'cyan'
+        }}
+      />
+    )
+  };
+
+class SearchBarExampleTwo extends Component {
+  render(){
+    return(
+      <SearchBar
+        onPress={
+          () => console.log('Do Stuff')
+        }
+        containerStyle={{
+          height:50,
+          marginHorizontal: 10,
+          borderRadius: 5,
+          borderColor: 'purple',
+          borderWidth: 1
+        }}
+        clearTextOnPress={false}
+        renderLeftIcon={false}
+        searchIconSize={20}
+        searchIconColor={'deeppink'}
+        searchIconBackgroundColor={'aqua'}
+        textStyle={{
+          color: 'cyan',
+          paddingLeft: 10
+        }}
+      />
+    )
+  };
+}
+```
---
+++ SearchBar.js
@@ -0,0 +1,100 @@
+'use strict'
+
+import React, { Component, PropTypes } from 'react';
+import {
+ TextInput,
+ View,
+ TouchableOpacity,
+} from 'react-native';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// 'onPress' will only be called only if there is a value in the text area, with value passed in
+
+const propTypes = {
+ onPress: React.PropTypes.func.isRequired,
+ containerStyle: React.PropTypes.object.isRequired,
+ clearTextOnPress: React.PropTypes.bool,
+ renderLeftIcon: React.PropTypes.bool,
+ searchIconSize: React.PropTypes.number,
+ searchIconColor: React.PropTypes.string,
+ searchIconBackgroundColor: React.PropTypes.string,
+ leftIconColor: React.PropTypes.string,
+ leftIconSize: React.PropTypes.number,
+ textStyle: React.PropTypes.object
+};
+
+export default class SearchBar extends Component {
+
+ constructor(){
+   super();
+   this.pressedSearchIcon = this.pressedSearchIcon.bind(this);
+   this.state = {
+     textInput: '',
+   };
+ };
+
+ pressedSearchIcon(){
+   if (this.state.textInput.length !== 0) {
+     this.props.onPress(this.state.textInput);
+   }
+   if (this.props.clearTextOnPress) {
+     this.setState({ textInput: '' });
+   }
+ };
+
+ renderLeftIcon(){
+   if (this.props.renderLeftIcon) {
+     return(
+         <TouchableOpacity
+           activeOpacity={1}
+           onPress={
+             () => this.textInput.focus()
+           }
+           style={{
+             flex:1,
+             alignItems:'center',
+             justifyContent:'center'
+           }}>
+           <Icon name={'search'} size={this.props.leftIconSize} color={this.props.leftIconColor} />
+         </TouchableOpacity>
+     )
+   }
+ };
+
+ render(){
+   return(
+     <View style={[{flexDirection:'row',overflow:'hidden'},this.props.containerStyle]}>
+       {this.renderLeftIcon()}
+       <View style={{
+         flex: 6,
+         backgroundColor: 'white',
+       }}>
+         <TextInput
+           ref={(ref) => this.textInput = ref}
+           placeholder={'Search'}
+           value={this.state.textInput}
+           onChangeText={
+             (text) => this.setState({textInput:text})
+           }
+           underlineColorAndroid={'rgba(0,0,0,0)'}
+           style={[{
+             flex: 1,
+           },this.props.textStyle]}/>
+       </View>
+       <TouchableOpacity
+         onPress={this.pressedSearchIcon}
+         style={{
+           flex: 1,
+           backgroundColor: this.props.searchIconBackgroundColor,
+           alignItems:'center',
+           justifyContent: 'center',
+         }}>
+         <Icon name={'search'} size={this.props.searchIconSize} color={this.props.searchIconColor} />
+       </TouchableOpacity>
+     </View>
+   )
+ };
+};
+
+SearchBar.propTypes = propTypes;
